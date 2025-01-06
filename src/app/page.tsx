f5a142cf84d5228ac573a62d6f59ca79c8847557
abdul-busybrain <awwalhajine@gmail.com>
2025-01-06 12:38:04 +0100

    Show logged in user info in Object

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import { Button, Input, ConfigProvider } from "antd";
 
-export default function Home() {
+export default async function Home() {
+  let email = "";
+  let name = "";
+
+  const user: any = await currentUser();
+  name = user?.fullName;
+  email = user?.emailAddresses[0].emailAddress;
+
   return (
     <ConfigProvider>
       <div className="p-5 flex flex-col gap-5">
         <h1>Elearn</h1>
-        <Button>Default button</Button>
-        <Button type="primary">Default button</Button>
-        <Input placeholder="Enter your name" />
+        <UserButton />
+        <h1>Name: {name}</h1>
+        <h1>Email: {email}</h1>
+        <h1>{`Clerk user ID: ${user.id}`}</h1>
       </div>
     </ConfigProvider>
   );
