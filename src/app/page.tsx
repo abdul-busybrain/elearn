65219a54bff215914b155f485adec8353eee9b8b
abdul-busybrain <awwalhajine@gmail.com>
2025-01-08 09:52:16 +0100

    Saved users in the MongoDB Server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,27 @@
+import { getCurrentUserFromMongoDB } from "@/server-actions/users";
 import { UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
-import { Button, Input, ConfigProvider } from "antd";
+import { Button, Input, ConfigProvider, Alert } from "antd";
 
 export default async function Home() {
   let email = "";
   let name = "";
 
-  const user: any = await currentUser();
-  name = user?.fullName;
-  email = user?.emailAddresses[0].emailAddress;
+  const response = await getCurrentUserFromMongoDB();
+  if (!response.success) {
+    return <Alert message={response.message} type="error" />;
+  }
+
+  name = response.data.name;
+  email = response.data.email;
 
   return (
-    <ConfigProvider>
-      <div className="p-5 flex flex-col gap-5">
-        <h1>Elearn</h1>
-        <UserButton />
-        <h1>Name: {name}</h1>
-        <h1>Email: {email}</h1>
-        <h1>{`Clerk user ID: ${user.id}`}</h1>
-      </div>
-    </ConfigProvider>
+    <div className="p-5 flex flex-col gap-5">
+      <h1>Elearn</h1>
+      <UserButton />
+      <h1>Name: {name}</h1>
+      <h1>Email: {email}</h1>
+      <h1>{`Clerk user ID: ${response.data.clerkUserId}`}</h1>
+    </div>
   );
 }
---
+++ src/interfaces/index.ts
@@ -0,0 +1,11 @@
+export interface IUSer {
+  _id: string;
+  name: string;
+  email: string;
+  clerkUserId: string;
+  profilePic: string;
+  isAdmin: boolean;
+  isActive: boolean;
+  createAt: string;
+  updatedAt: string;
+}
